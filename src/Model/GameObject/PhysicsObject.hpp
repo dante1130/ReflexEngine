cadc6d4a0faf6aa726e2b4e68668b61fb898be0b
dandria <80442706+dandria56@users.noreply.github.com>
2022-04-16 12:52:03 +0800

    physics object doxygen added

--- src/Model/GameObject/PhysicsObject.hpp
+++ src/Model/GameObject/PhysicsObject.hpp
@@ -7,17 +7,69 @@
 
 class PhysicsObject : public BodyRigid {
 public:
+	/**
+	 * @brief	Default constructor for physics object
+	 * @param	No param
+	 * @return	No return
+	 *
+	 * @pre	Nothing
+	 * @post	Object created
+	 */
 	PhysicsObject();
 
+	/**
+	 * @brief	Initialised the model
+	 * @param	model_path		- path to model
+	 * @param	shininess		- shininess
+	 * @param	spec_intensity	- specual intensity
+	 * @return	No return
+	 *
+	 * @pre		Object exists
+	 * @post	model initialised
+	 */
 	void initModel(const std::string& model_path, GLfloat shininess,
 	               GLfloat spec_intensity);
 
+	/**
+	 * @brief	Initialised the rigid body
+	 * @param	pos			- position of the rigid body
+	 * @param	rotation	- axis of rotation of the rigid body
+	 * @param	angle		- Rotation around axis of rotation of rigid body
+	 * @return	No return
+	 *
+	 * @pre		Object exists
+	 * @post	rigid body initialised
+	 */
 	void initRB(glm::vec3 pos, glm::vec3 rotation, float angle);
 
+	/**
+	 * @brief	Updates the object
+	 * @param	delta_time	- Time since last frame
+	 * @return	No return
+	 *
+	 * @pre		rigid body initialised
+	 * @post	Object updated
+	 */
 	void update(float delta_time) override;
 
+	/**
+	 * @brief	Adds draw call of object
+	 * @param	No param
+	 * @return	No return
+	 *
+	 * @pre		Model initialised
+	 * @post	Draw call added
+	 */
 	void add_draw_call() override;
 
+	/**
+	 * @brief	Draws the object
+	 * @param	shader	- The shader
+	 * @return	No return
+	 *
+	 * @pre		Model initialised
+	 * @post	Model drawn
+	 */
 	void draw(std::shared_ptr<Shader> shader) override;
 
 private:
