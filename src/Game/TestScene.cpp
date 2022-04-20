3804daed11bbf7b6bafcb68e01d56b7967d3106d
Andrew <andrewdante2@gmail.com>
2022-04-20 23:00:52 +0800

    Moved flashlight to player object

--- src/Game/TestScene.cpp
+++ src/Game/TestScene.cpp
@@ -11,11 +11,6 @@ void TestScene::init() {
 	    DirectionalLight(2048, 2048, glm::vec3(1.0f, 0.53f, 0.3f), 0.2f,
 	                     glm::vec3(-10.0f, -12.0f, 18.5f), 0.9f);
 
-	flashlight_ =
-	    SpotLight(1024, 1024, 0.01f, 100.0f, glm::vec3(1.0f, 1.0f, 1.0f), 0.0f,
-	              2.0f, glm::vec3(0.0f, 0.0f, 0.0f),
-	              glm::vec3(0.0f, -1.0f, 0.0f), 1.0f, 0.0f, 0.0f, 20.0f);
-
 	sol::state& lua = LuaManager::get_instance().get_state();
 	GenericFunctions::lua_access();
 
@@ -93,7 +88,6 @@ void TestScene::add_draw_call() {
 	auto& renderer = ReflexEngine::get_instance().renderer_;
 
 	renderer.add_directional_light(directional_light_);
-	renderer.add_spot_light(flashlight_);
 
 	for (auto& game_object : game_objects_) {
 		game_object->add_draw_call();
@@ -103,15 +97,6 @@ void TestScene::add_draw_call() {
 void TestScene::update(float delta_time) {
 	garbage_collection();
 	add_new_game_objects();
-
-	const auto& camera = ReflexEngine::get_instance().camera_;
-
-	glm::vec3 lower_light = camera.get_position();
-	lower_light.y -= 0.3f;
-
-	glm::vec3 cam_direction = camera.get_direction();
-
-	flashlight_.SetFlash(lower_light, cam_direction);
 
 	for (auto& game_object : game_objects_) {
 		game_object->update(delta_time);
