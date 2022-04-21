8c39a4a7949ef885915f644aa548675cc15154db
openGLBRUH <SpencerShawJoe@outlook.com>
2022-04-21 22:25:25 +0800

    Doxygen update

--- src/Controller/Terrain/BTT.hpp
+++ src/Controller/Terrain/BTT.hpp
@@ -6,77 +6,188 @@
 #include <iostream>
 
 struct Node {
-	// holds indexes
-	glm::vec3 point;
+	
+	glm::vec3 point; ///Holds indices for given triangle
 
 	// Children of triangle
 	Node* left;
 	Node* right;
 
-	unsigned int lod;
+	unsigned int lod; ///Level of detail for given triangle
 };
 
-using Function = void (*)(glm::vec3&);
-
+/**
+ * @class Binary Triangle Tree class
+ */
 class Btt {
 public:
 	Btt();  /// Default constructor
 
-	Btt(const Btt& other);
+	Btt(const Btt& other); ///Copy constructor for BTT
 
-	Btt& operator=(const Btt& treeCopy);
+	Btt& operator=(const Btt& treeCopy);  /// equals operator overloader for BTT
 
-	~Btt();
+	~Btt(); ///Deconstructor for BTT 
 
-	void Insert(unsigned int size, bool isFlip);
-
+	/**
+	 * @brief	Creates a BTT based of given indices, detail size and whether
+	 * BTT is North-East type or South-West type (i.e is it flipped)
+	 *
+	 * @param	temp - starting indices, indices of largest triangle (level of
+	 * detail 1)
+	 * @param	size - length/width of a tile (in vertices)
+	 * @param	isFlip -  indicates whether given BTT is North-East or South-West
+	 * triangle type triangle type or South-West triangle type
+	 *
+	 * @return void
+	 */
 	void Insert(const glm::vec3& temp, unsigned int size, bool isFlip);
 
+	/**
+	 * @brief	Returns all indices needed for a given level of detail
+	 *
+	 * @param	level - Level of detail needed for BTT
+	 *
+	 * @return vector of indices for triangles
+	 */
 	const std::vector<glm::vec3>& getIndices(const int level);
 
+	/**
+	 * @brief	Gets the size of current indices
+	 *
+	 * @return 	size of current indices
+	 */
 	const GLuint getIndexSize();
 
+	/**
+	 * @brief	Gets the current level of detail
+	 *
+	 * @return  returns current level of detail
+	 */
 	unsigned int getLOD();
 
+	/**
+	 * @brief	Destroys a node of tree
+	 *
+	 * @return void
+	 */
 	void destroyTree();
 
+	/**
+	 * @brief	Gets all the current indices stored in BTT
+	 *
+	 * @return all of the currently used indices
+	 */
 	const std::vector<glm::vec3>& getCurrentIndices();
 
+	/**
+	 * @brief	Adds lower levels of detail triangles that touch left edges
+	 * to current indices, used for removing t-junctions
+	 *
+	 * @return void
+	 */
 	void addLeft();
 
+	/**
+	 * @brief	Adds lower levels of detail triangles that touch left edges
+	 * to current indices, used for removing t-junctions
+	 *
+	 * @return void
+	 */
 	void addRight();
 
 private:
+	/**
+	 * @brief	Creates a node to add into BTT
+	 *
+	 * @param	lod - level of detail
+	 * @param 	data - indices for a given triangle
+	 *
+	 * @return pointer to created node
+	 */
 	Node* createNode(const unsigned int& lod, const glm::vec3& data);
 
+	/**
+	 * @brief	Searches through tree and creates copies of nodes
+	 *
+	 * @param	nodeCopy - pointer address of original node
+	 * @param	node - pointer to new tree/tree nodes being created
+	 *
+	 * @return void
+	 */
 	void copyTree(Node*& nodeCopy, Node* node);
 
+	/**
+	 * @brief	Inserts a node into BTT
+	 *
+	 * @param	node - current node being used
+	 *
+	 * @return void
+	 */
 	void Insert(Node* node);
 
+	/**
+	 * @brief	Gets the indices for a given node and adds to current indices
+	 *
+	 * @param	node - current node being used
+	 * @param	level - level of detail being used for extraction
+	 *
+	 * @return void
+	 */
 	const void getIndices(Node* node, int level);
 
+	/**
+	 * @brief	Deletes a given node of a tree
+	 *
+	 * @param	node - current node being used
+	 *
+	 * @return void
+	 */
 	void destroyTree(Node* node);
 
+	/**
+	 * @brief	Finds the midpoint between two indices
+	 *
+	 * @param	p1 - left most indice
+	 * @param	p2 - right most indice
+	 *
+	 * @return void
+	 */
 	GLuint findMid(unsigned int p1, unsigned int p2);
 
+	/**
+	 * @brief	Checks if a triangle should be added to left side triangles
+	 *
+	 * @param	node - current node being used
+	 * @param 	i - the index of node in current indices
+	 *
+	 * @return void
+	 */
 	void addToLeft(Node* node, const int& i);
 
+	/**
+	 * @brief	Checks if a triangle should be added to left side triangles
+	 *
+	 * @param	node - current node being used
+	 * @param 	i - the index of node in current indices
+	 *
+	 * @return void
+	 */
 	void addToRight(Node* node, const int& i);
 
-	Node* bttNode;  // holds indices for triangles
+	Node* bttNode;				/// Holds indices for all triangles
 
-	Btt* leftTriangle;  // touching left side of main triangle (origin and left)
-	Btt* rightTriangle;  // touching right side of main triangle (origin and
-	                     // right)
+	Btt* leftTriangle;			/// BTT touching left side of main triangle (origin and left)
+	Btt* rightTriangle;			/// BTT touching right side of main triangle (origin and right)
 
-	// vectors of triangles that hypotenus touch main triangle smaller sides
-	std::vector<glm::vec3> leftSideTriangles;
-	std::vector<glm::vec3> rightSideTriangles;
-	std::vector<glm::vec3> currentIndices;
-	std::vector<int> bigLeftTriangles;
-	std::vector<int> bigRightTriangles;
+	
+	std::vector<glm::vec3> leftSideTriangles;	/// Stores triangles touching left side (only stores for certain LOD)
+	std::vector<glm::vec3> rightSideTriangles;	/// Stores triangles touching right side (only stores for certain LOD)
+	std::vector<glm::vec3> currentIndices;		/// Current indices being used
+	std::vector<int> bigLeftTriangles;			/// Holds indexs for left side triangles inside current indices
+	std::vector<int> bigRightTriangles;			/// Holds indexs for right side triangles inside current indices
 
-	unsigned int currentLod;
-	unsigned int depth;  // stores depth
-	bool isFlipped;
+	unsigned int currentLod;					/// The current level of detail
+	unsigned int depth;							/// Stores total depth
+	bool isFlipped;								/// Whether BTT is North-East or South-West triangle type
 };
--- src/Controller/Terrain/BttController.hpp
+++ src/Controller/Terrain/BttController.hpp
@@ -2,13 +2,16 @@
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
-//#include <vector>
 #include <GLFW/glfw3.h>
 
 #include "Terrain.hpp"
 #include "View/Renderer/OpenGL/Objects/Mesh.hpp"
 #include "BTT.hpp"
 
+
+/**
+ * @class Binary Triangle Tree Controller class
+ */
 class BttController : public Terrain {
 public:
 	void render(std::shared_ptr<Shader> shader) override;
@@ -21,20 +24,74 @@ public:
 	 */
 	bool load_mesh() override;
 
+	/**
+	 * @brief Default contructor for BttController.
+	 */
 	BttController();
 
+	/**
+	 * @brief Decontructor for BttController.
+	 */
 	~BttController();
 
+	/**
+	 * @brief	Stores the psoition of the camera within the BttController
+	 * for later use
+	 *
+	 * @param		pos	- The position of the camera
+	 */
 	void getCameraPosition(const glm::vec3& pos);
 
+	/**
+	 * @brief	Initialises the BTT grid by generating vertices, creating BTT
+	 * structures defining centers and generating LOD distances
+	 *
+	 * @param		chunkSize - The length of the grid i.e chunkSize * chunkSize is
+	 * equal to number of tiles displayed
+	 * @param		chunkDetail - The amount of vertices per row of a tile i.e
+	 * chunkDetail * chunkDetail is equal to total number of vertice sper tile
+	 * @param		n - Factor that determining tile detail i.e chunkDetail
+	 * must be equal to 2^n + 1
+	 *
+	 * @return void
+	 */
 	void CreateTerrain(int chunkSize, int chunkDetail, int n);
 
+	/**
+	 * @brief	Searches through BTT and determines if a BTT needs to increase
+	 * its LOD to remove potential T-Junction (NOT IMPLEMENTED)
+	 *
+	 * @return void
+	 */
 	void checkTJunction();
 
+	
+	/**
+	 * @brief	Updates the mesh when the player moves position
+	 *
+	 * @param	player - The players position in world coordinates
+	 *
+	 * @return void
+	 */
 	void Update(glm::vec3 player);
 
+	/**
+	 * @brief	Calculates the meshes new indices for all BTTs and stores them for
+	 * later use
+	 *
+	 * @param	pos - The players position in world coordinates
+	 *
+	 * @return void
+	 */
 	void getAllIndices(glm::vec3 pos);
 
+	/**
+	 * @brief	Sets the size of the heightmap
+	 *
+	 * @param	size - Sets length and width of heightmap
+	 *
+	 * @return void
+	 */
 	void set_height_map_size(float size);
 
 	/**
@@ -57,33 +114,52 @@ public:
 
 private:
 	
+	/**
+	 * @brief	Gets the array of BTT tile centers
+	 *
+	 * @return a list of vector2
+	 */
 	const std::vector<glm::vec2>& getCenters();
 
+	/**
+	 * @brief	Generate all vertices for terrain
+	 *
+	 * @param	chunkSize - Determines the gridsize of BTTs
+	 * @param 	chunkDetail - Determines the no of vertices per BTT
+	 *
+	 * @return void
+	 */
 	void GenerateVertices(int chunkSize, int chunkDetail);
 
+	/**
+	 * @brief	based off player position, determines a BTT's level of detail
+	 *
+	 * @param	pos - The players position in world coordinates
+	 * @param 	center - The center of a tile in world coordinates
+	 *
+	 * @return the level of detail calculated
+	 */
 	const GLuint findLOD(glm::vec3 pos, glm::vec2 center);
 
-	std::vector<std::vector<Btt>> bttMap;  // all binary trees containing
-	                                       // indices
-	std::vector<glm::vec2> bttCenters;     // all
-	std::vector<GLfloat> vertices;
-	std::vector<GLuint> indices;
-	glm::vec3 cam;
+	std::vector<std::vector<Btt>> bttMap;   /// All binary triangle trees
+	std::vector<glm::vec2> bttCenters;      /// All centers for BTT tiles (hypotenuse-adjacent BTTs)
+	std::vector<GLfloat> vertices;			/// All the vertices for the terrain
+	std::vector<GLuint> indices;			/// All the currently used indices for terrain
+	glm::vec3 cam;							/// Stored position for camera
 
-	std::vector<GLfloat> distances;
+	std::vector<GLfloat> distances;			/// Distances from player for changes in LOD for tile
 
-	std::shared_ptr<Mesh> mesh_ = nullptr;
-	bool firstRun;
 
-	glm::vec3 prev_pos;
-	GLuint current_total_indices;
-	GLuint chunk_size;
-	GLuint chunk_detail;
+	std::shared_ptr<Mesh> mesh_ = nullptr;  /// Container for mesh data;
+	bool firstRun;							/// Determines if first time of mesh generation
 
-	int height_map_size = 0;
+	glm::vec3 prev_pos;						/// Saves the players previous position
+	GLuint current_total_indices;			/// Number of current total indices being used
+	GLuint chunk_size;						/// The length/width of BTT grid
+	GLuint chunk_detail;					/// The length/width of a given tile (vertice number)
 
-	/// The texture.
-	std::shared_ptr<Texture> texture_ = nullptr;
-	/// The detailmap.
-	std::shared_ptr<Texture> detailmap = nullptr;
+	int height_map_size = 0;				/// the length/width of height map
+
+	std::shared_ptr<Texture> texture_ = nullptr; /// Storage for texture.
+	std::shared_ptr<Texture> detailmap = nullptr; /// Storage for detailmap.
 };
