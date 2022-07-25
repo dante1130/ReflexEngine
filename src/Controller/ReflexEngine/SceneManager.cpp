9f7574aab04839abded7cad50d46a59421df1dc7
Andrew <andrewdante2@gmail.com>
2022-07-25 12:57:47 +0800

    Doxygen comments for scene manager

--- src/Controller/ReflexEngine/SceneManager.cpp
+++ src/Controller/ReflexEngine/SceneManager.cpp
@@ -5,8 +5,11 @@
 void SceneManager::lua_access() {
 	auto& lua = LuaManager::get_instance().get_state();
 
-	lua.set_function("create_scene", &SceneManager::create_scene, this);
-	lua.set_function("load_scene", &SceneManager::load_scene, this);
+	auto scene_table = lua["Scene"].get_or_create<sol::table>();
+
+	scene_table.set_function("create_scene", &SceneManager::create_scene, this);
+	scene_table.set_function("load_scene", &SceneManager::load_scene, this);
+	scene_table.set_function("reset_scene", &SceneManager::reset_scene, this);
 }
 
 void SceneManager::create_scene(const std::string& name,
@@ -20,7 +23,7 @@ void SceneManager::load_scene(const std::string& name) {
 
 	auto& lua = LuaManager::get_instance().get_state();
 
-	auto scene_table = lua["Scene"];
+	auto scene_table = lua["Scene"].get_or_create<sol::table>();
 
 	lua["add_game_object"] = [this](const std::string& lua_script) {
 		current_scene_->add_game_object(lua_script);
@@ -33,4 +36,7 @@ void SceneManager::reset_scene(const std::string& name) {
 	scene.init();
 }
 
-ECSScene& SceneManager::current_scene() const { return *current_scene_; }
+ECSScene& SceneManager::current_scene() const {
+	assert(current_scene_ != nullptr, "No scene loaded!");
+	return *current_scene_;
+}
--- src/Controller/ReflexEngine/SceneManager.hpp
+++ src/Controller/ReflexEngine/SceneManager.hpp
@@ -5,22 +5,56 @@
 
 #include "Scene/ECSScene.hpp"
 
+/**
+ * @class SceneManager
+ * @brief The scene manager.
+ */
 class SceneManager {
 public:
 	SceneManager() = default;
 
+	/**
+	 * @brief Register the scene manager to lua.
+	 */
 	void lua_access();
 
+	/**
+	 * @brief Create a scene with a given name and its master lua script.
+	 *
+	 * @param name The name of the scene.
+	 * @param master_lua_script The master lua script of the scene.
+	 */
 	void create_scene(const std::string& name,
 	                  const std::string& master_lua_script);
 
+	/**
+	 * @brief Load a scene with a given name.
+	 *
+	 * @param name The name of the scene.
+	 *
+	 * @pre The scene with the given name must exist.
+	 * @post The scene with the given name is loaded, and usage of
+	 * Scene.add_game_object is directed to the scene loaded.
+	 */
 	void load_scene(const std::string& name);
 
+	/**
+	 * @brief Reset a scene with a given name.
+	 *
+	 * @param name The name of the scene.
+	 */
 	void reset_scene(const std::string& name);
 
+	/**
+	 * @brief Get the current scene.
+	 *
+	 * @return ECSScene&
+	 */
 	ECSScene& current_scene() const;
 
 private:
+	/// The map of scenes.
 	std::unordered_map<std::string, ECSScene> scene_map_;
-	ECSScene* current_scene_;
+	/// The current scene.
+	ECSScene* current_scene_ = nullptr;
 };
