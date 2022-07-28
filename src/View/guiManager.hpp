762519a09e7c619f7ae3e546cef85bb834d2baba
Andrew <andrewdante2@gmail.com>
2022-07-28 11:47:34 +0800

    Add CMakeLists linking for ImGui

--- src/View/guiManager.hpp
+++ src/View/guiManager.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include <string>
 #include <map>
