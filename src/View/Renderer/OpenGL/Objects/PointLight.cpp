298daea6cb1ccd88bc4a7862f6f1a985b20623ea
Andrew <andrewdante2@gmail.com>
2022-08-11 14:52:19 +0800

    Add OmniShadowMap class to point and spot lights

--- src/View/Renderer/OpenGL/Objects/PointLight.cpp
+++ src/View/Renderer/OpenGL/Objects/PointLight.cpp
@@ -1,13 +1,23 @@
 #include "PointLight.hpp"
 
-PointLight::PointLight(glm::vec3 color, GLfloat aIntensity, GLfloat dIntensity,
-                       glm::vec3 position, GLfloat constant, GLfloat linear,
-                       GLfloat quadratic)
-    : Light(1024, 1024, color, aIntensity, dIntensity),
+PointLight::PointLight(GLuint shadow_width, GLuint shadow_height, GLfloat near,
+                       GLfloat far, glm::vec3 color, GLfloat aIntensity,
+                       GLfloat dIntensity, glm::vec3 position, GLfloat constant,
+                       GLfloat linear, GLfloat quadratic)
+    : Light(color, aIntensity, dIntensity),
+      far_plane_(far),
       m_position(position),
       m_constant(constant),
       m_linear(linear),
-      m_quadratic(quadratic) {}
+      m_quadratic(quadratic) {
+	float aspect =
+	    static_cast<float>(shadow_width) / static_cast<float>(shadow_height);
+
+	light_projection_ =
+	    glm::perspective(glm::radians(90.0f), aspect, near, far);
+
+	omni_shadow_map_.init(shadow_width, shadow_height);
+}
 
 void PointLight::UseLight(GLuint ambientColorLoc, GLuint ambientIntensityLoc,
                           GLuint diffuseIntensityLoc, GLuint positionLoc,
@@ -23,6 +33,42 @@ void PointLight::UseLight(GLuint ambientColorLoc, GLuint ambientIntensityLoc,
 	glUniform1f(quadraticLoc, m_quadratic);
 }
 
+std::vector<glm::mat4> PointLight::calculate_light_transforms() const {
+	std::vector<glm::mat4> light_matrices;
+
+	// +x, -x
+	light_matrices.emplace_back(
+	    light_projection_ *
+	    glm::lookAt(m_position, m_position + glm::vec3(1.0f, 0.0f, 0.0f),
+	                glm::vec3(0.0f, -1.0f, 0.0f)));
+	light_matrices.emplace_back(
+	    light_projection_ *
+	    glm::lookAt(m_position, m_position + glm::vec3(-1.0f, 0.0f, 0.0f),
+	                glm::vec3(0.0f, -1.0f, 0.0f)));
+
+	// +y, -y
+	light_matrices.emplace_back(
+	    light_projection_ *
+	    glm::lookAt(m_position, m_position + glm::vec3(0.0f, 1.0f, 0.0f),
+	                glm::vec3(0.0f, 0.0f, 1.0f)));
+	light_matrices.emplace_back(
+	    light_projection_ *
+	    glm::lookAt(m_position, m_position + glm::vec3(0.0f, -1.0f, 0.0f),
+	                glm::vec3(0.0f, 0.0f, -1.0f)));
+
+	// +z, -z
+	light_matrices.emplace_back(
+	    light_projection_ *
+	    glm::lookAt(m_position, m_position + glm::vec3(0.0f, 0.0f, 1.0f),
+	                glm::vec3(0.0f, -1.0f, 0.0f)));
+	light_matrices.emplace_back(
+	    light_projection_ *
+	    glm::lookAt(m_position, m_position + glm::vec3(0.0f, 0.0f, -1.0f),
+	                glm::vec3(0.0f, -1.0f, 0.0f)));
+
+	return light_matrices;
+}
+
 void PointLight::set_point_light(glm::vec3 color, GLfloat aIntensity,
                                  GLfloat dIntensity, glm::vec3 position,
                                  GLfloat constant, GLfloat linear,
@@ -36,4 +82,8 @@ void PointLight::set_point_light(glm::vec3 color, GLfloat aIntensity,
 	m_quadratic = quadratic;
 }
 
-glm::vec3 PointLight::GetPosition() const { return m_position; }
+const OmniShadowMap& PointLight::get_shadow_map() const {
+	return omni_shadow_map_;
+}
+
+GLfloat PointLight::get_far_plane() const { return far_plane_; }
