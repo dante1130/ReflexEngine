7eaf0086c131747dec50a2700467f5a749318367
Andrew <andrewdante2@gmail.com>
2022-08-18 10:23:36 +0800

    Unit testing working build with all dependencies

---
+++ test/shader_test.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "View/Renderer/OpenGL/Objects/Shader.hpp"
+
+TEST_CASE("Shader uniforms") {
+	SECTION("Default shader uniforms") {
+		Shader shader;
+
+		REQUIRE(true);
+	}
+}
