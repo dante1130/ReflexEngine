2e61a74e35b2b6b88e3f556941c2e3b491d29280
dante1130 <andrewdante2@gmail.com>
2022-08-24 10:36:21 +0800

    Adjusted maximum point and spot lights to be within 32 texture units

--- src/View/Renderer/OpenGL/Objects/Shader.cpp
+++ src/View/Renderer/OpenGL/Objects/Shader.cpp
@@ -353,11 +353,12 @@ void Shader::CompileProgram() {
 	for (size_t i = 0; i < MAX_POINT_LIGHTS + MAX_SPOT_LIGHTS; ++i) {
 		char locBuff[100] = {'\0'};
 
-		snprintf(locBuff, sizeof(locBuff), "omni_shadow_maps[%d]", i);
+		snprintf(locBuff, sizeof(locBuff), "omni_shadow_maps[%d].shadow_map",
+		         i);
 		uniformOmniShadowMap[i].shadowMap =
 		    glGetUniformLocation(m_shaderID, locBuff);
 
-		snprintf(locBuff, sizeof(locBuff), "omni_far_planes[%d]", i);
+		snprintf(locBuff, sizeof(locBuff), "omni_shadow_maps[%d].far_plane", i);
 		uniformOmniShadowMap[i].farPlane =
 		    glGetUniformLocation(m_shaderID, locBuff);
 	}
