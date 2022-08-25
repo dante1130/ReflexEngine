aaff3809142c3a81d6e484bdfaa3639ba6034abf
Andrew <andrewdante2@gmail.com>
2022-08-25 17:57:42 +0800

    Added more lights

---
+++ resources/game/TheDen/prefabs/lights/left_lights/left_light1.lua
@@ -0,0 +1,29 @@
+entity = {
+	name = "Left corridor light 1",
+
+	point_light = {
+		shadow_width = 1024,
+		shadow_height = 1024,
+		near_plane = 0.1,
+		far_plane = 100,
+
+		color = {
+			r = 1,
+			g = 1,
+			b = 1
+		},
+
+		ambient_intensity = 0,
+		diffuse_intensity = 1,
+
+		position = {
+			x = 40,
+			y = 2.5,
+			z = 1
+		},
+
+		constant = 0,
+		linear = 0,
+		quadratic = 1
+	}
+}
