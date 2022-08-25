f5341ce6f13b1fb79e81893c4e90dbc55c3d538a
Andrew <andrewdante2@gmail.com>
2022-08-25 12:39:04 +0800

    Adjusted sunlight orthogonal projection

--- resources/game/TheDen/prefabs/lights/sunlight.lua
+++ resources/game/TheDen/prefabs/lights/sunlight.lua
@@ -4,10 +4,10 @@ entity = {
 	directional_light = {
 		shadow_width = 1024,
 		shadow_height = 1024,
-		near_plane = 1.0,
+		near_plane = 0.01,
 		far_plane = 256.0,
-		ortho_left = -64.0,
-		ortho_right = 64.0,
+		ortho_left = -128.0,
+		ortho_right = 0.0,
 		ortho_bottom = -64.0,
 		ortho_top = 64.0,
 
