010ab673f0779ad04e888f06c45534861cfc64a9
dandria <80442706+dandria56@users.noreply.github.com>
2022-10-03 15:02:51 +0800

    removed current physics system

--- src/Controller/Physics/CollisionEvent.hpp
+++ src/Controller/Physics/CollisionEvent.hpp
@@ -1,18 +1,3 @@
 #pragma once
 
 #include <reactphysics3d/reactphysics3d.h>
-
-/**
- * @author Andrew Ho
- * @class CollisionEventListener
- * @brief An event listener for reactphysics3d collision events to retrieve the
- * collision and contact points data.
- */
-class CollisionEventListener : public rp3d::EventListener {
-	/**
-	 * @brief The onContact method is called when a contact is detected.
-	 *
-	 * @param collision_data The collision data containing the contact points.
-	 */
-	virtual void onContact(const CallbackData& collision_data) override;
-};
