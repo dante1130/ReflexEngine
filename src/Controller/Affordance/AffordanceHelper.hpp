f1a1dbe11f37c2f441a48221f8f6cf5be288ba10
Andrew <andrewdante2@gmail.com>
2022-10-25 01:08:33 +0800

    Add doxygen comments to find_affordance

--- src/Controller/Affordance/AffordanceHelper.hpp
+++ src/Controller/Affordance/AffordanceHelper.hpp
@@ -3,6 +3,17 @@
 #include "Affordance.hpp"
 
 namespace Affordance {
+/**
+ * @brief Find the best affordance from a list of affordances.
+ * Determines the best affordance by comparing the properties of the affordances
+ * with the given properties. The properties are weighted by the given weight,
+ * if it is not given, the weight is 1.
+ *
+ * @param affordances The list of affordances to search.
+ * @param properties The properties to search for.
+ * @param properties_weight The weight of each property.
+ * @return AffordancePtr The best affordance
+ */
 auto find_affordance(const AffordancePtr& affordances,
                      const Properties& properties,
                      const PropertiesWeight& properties_weight)
