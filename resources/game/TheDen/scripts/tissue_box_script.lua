b89b59f8a332237859ffbae74e9f4f504a86b215
dandria <80442706+dandria56@users.noreply.github.com>
2022-10-30 18:30:03 +0800

    added back tissue box script

--- resources/game/TheDen/scripts/tissue_box_script.lua
+++ resources/game/TheDen/scripts/tissue_box_script.lua
@@ -2,17 +2,13 @@ var = {
 }
 
 function init(ecs, entity)
-	if(not entity:any_rigidbody_component()) then
-		entity:add_rigidbody_component()
-	end
 	local rb = entity:get_rigidbody_component()
-	rb.type = 0
-	LoadOBJCollider(rb, "models/TheDenCollisions.obj")
+	rb:add_box_collider(Math.vec3.new(0, 0, 0), Math.vec3.new(0, 0, 0), Math.vec3.new(0.2, 0.12, 0.12), 0.5, 0.2, 0.25)
 
 	local script = entity:get_script_component()
 	script.is_active = false;
 end
 
 function update(ecs, entity)
-
+	
 end
