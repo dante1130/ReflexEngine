c5bfb5bee0932ee485e58f3a60c9a017cd4c977b
dandria <80442706+dandria56@users.noreply.github.com>
2022-11-02 13:38:51 +0800

    added TV model and removed ability to switch scenes

---
+++ resources/game/TheDen/prefabs/screen.lua
@@ -0,0 +1,26 @@
+entity = {
+	name = "TV",
+
+	transform = {
+		position = {
+			x = 64.6,
+			y = 1.4,
+			z = -5.3
+		},
+		rotation = {
+			x = 0,
+			y = 0,
+			z = 0
+		},
+		scale = {
+			x = 1,
+			y = 1,
+			z = 1
+		}
+	},
+
+	model = {
+		model_name = "tv",
+		material_name = "default"
+	}
+}
